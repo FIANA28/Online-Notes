8a04b52cc8330dfd00a826e51328284751c1a0a6
FIANA28 <44801231+FIANA28@users.noreply.github.com>
2020-11-30 09:12:58 +0200

    Update connection.php
    add azure connection

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
+//localhost version
 // <!-- Connect to the database  -->
 // $link = mysqli_connect("localhost", "root", "root", "onlinenotes");
 // checking if database is connected
@@ -6,4 +7,12 @@
     // die('ERROR: Unable to connect:' . mysqli_connect_error());
     // echo "<script>window.alert('Connection succesfull!')</script>";
 // }
+//Azure version
+// Connect to the database 
+$link = mysqli_connect("127.0.0.1:50735", "azure", "6#vWHD_$", "onlinenotes");
+// checking if database is connected
+if(mysqli_connect_error()){
+    die('ERROR: Unable to connect:' . mysqli_connect_error());
+    echo "<script>window.alert('Connection succesfull!')</script>";
+}
 ?>
